d7d96f82a5592897c61f714f34c89c5227f774a4
Erin Eustice <erinrenee.va@gmail.com>
2019-11-23 20:33:06 -0700

    Cash Register with changes to cashier return

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class CashRegister {
-	static int makeChange;
-	int changeDue;
+	Scanner kb = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
+		double changeDue;
 
 		System.out.println("What Is The Price Of The Item? ");
 		double amnt = kb.nextDouble();
@@ -14,82 +14,160 @@ public class CashRegister {
 		double tendered = kb.nextDouble();
 		kb.close();
 
-		int changeDue = (int) (((tendered * 100) - (amnt * 100))+0.001);
-		double changE = ((double) changeDue) / 100;
+		changeDue = (tendered - amnt);
+		
 
 		if (tendered < amnt) {
+			System.out.println("Price of item:" + amnt);
+			System.out.println("Amount customer tendered: " + tendered);
+			System.out.println("__________________________");
 			System.out.println("ERROR; insufficient amount.");
 
 		} else if (tendered == amnt) {
+			System.out.println("Price of item:" + amnt);
+			System.out.println("Amount customer tendered: " + tendered);
+			System.out.println("____________________________");
 			System.out.println("Change due: $0.00. Thank you!  ");
 
-		}
-		if (tendered > amnt) {
-			System.out.println(" change due : $" + changE + "0");
+		} else { // (tendered > amnt) {
+			System.out.println("Price of item:" + amnt);
+			System.out.println("Amount customer tendered: " + tendered);
+			System.out.println("_______________________________");
+			System.out.println(" change due : $" + changeDue + "0");
+			System.out.println();
+			System.out.println("Cashier return:  ");
 
 			makeChange(changeDue);
-			
+
 		}
 	}
 
-	public static int makeChange(int changeDue) {
+	public static void makeChange(double changeDue) {
 
-		int penny = 1;
+		double penny = .01;
 		int pennycount = 0;
-		int nickel = 5;
+		double nickel = .05;
 		int nickelcount = 0;
-		int dime = 10;
+		double dime = .10;
 		int dimecount = 0;
-		int quarter = 25;
+		double quarter = .25;
 		int quartercount = 0;
-		int dollar = 100;
+		int dollar = 1;
 		int dollarcount = 0;
-		int five = 500;
+		int five = 5;
 		int fivecount = 0;
-		int ten = 1000;
+		int ten = 10;
 		int tencount = 0;
+		int twenty = 20;
+		int twentycount = 0;
+		int hundred = 100;
+		int hundredcount = 0;
 
-		
-		while (ten < changeDue) {
+		while (changeDue >= hundred) {
+			hundredcount++;
+			changeDue = changeDue - hundred;
+		}
+		if (hundredcount == 1) {
+			System.out.println(hundredcount + " hundred");
+		} else if (twentycount > 1) {
+			System.out.println(hundredcount + " hundreds");
+		}
+
+		while (changeDue >= twenty) {
+			twentycount++;
+			changeDue = changeDue - twenty;
+		}
+		if (twentycount == 1) {
+			System.out.println(twentycount + " twenty");
+		} else if (twentycount > 1) {
+			System.out.println(twentycount + " twentys");
+		}
+
+		while (changeDue >= ten) {
 			tencount++;
 			changeDue = changeDue - ten;
-			System.out.println(tencount + "ten dollar bills");
-			
 		}
-		 if (five < changeDue) {
+		if (tencount == 1) {
+			System.out.println(tencount + " ten");
+		} else if (tencount > 1) {
+			System.out.println(tencount + " tens");
+		}
+
+		while (changeDue >= five) {
 			fivecount++;
 			changeDue = changeDue - five;
-			System.out.println(fivecount + " Five Dollar Bills");
 		}
-		if (dollar < changeDue) {
+		if (fivecount == 1) {
+			System.out.println(fivecount + " five");
+		} else if (fivecount > 1) {
+			System.out.println(fivecount + " fives");
+		}
+
+		while (changeDue >= dollar) {
 			dollarcount++;
 			changeDue = changeDue - dollar;
-			System.out.println(dollarcount + " One Dollar Bills");
 		}
-		if (quarter < changeDue) {
+		if (dollarcount == 1) {
+			System.out.println(dollarcount + " dollar");
+		} else if (dollarcount > 1) {
+			System.out.println(dollarcount + " dollars");
+		}
+
+		while (changeDue >= quarter) {
 			quartercount++;
 			changeDue = changeDue - quarter;
+		}
+		if (quartercount == 1) {
+			System.out.println(quartercount + " quarter");
+		} else if (quartercount > 0) {
 			System.out.println(quartercount + " quarters");
 		}
-		if (dime < changeDue) {
+
+		while (changeDue >= dime) {
 			dimecount++;
 			changeDue = changeDue - dime;
-			System.out.println(dimecount + " dime(s)");
 		}
-		if (nickel < changeDue) {
+		if (dimecount == 1) {
+			System.out.println(dimecount + " dime");
+		} else if (dimecount > 0) {
+			System.out.println(dimecount + " dimes");
+		}
+
+		while (changeDue >= nickel) {
 			nickelcount++;
 			changeDue = changeDue - nickel;
+		}
+		if (nickelcount == 1) {
+			System.out.println(nickelcount + " nickel");
+		} else if (nickelcount > 0) {
 			System.out.println(nickelcount + " nickels");
 		}
-		if (penny <= (changeDue)) {
+
+		while (changeDue >= penny) {
 			pennycount++;
-			changeDue = changeDue - penny;
-			System.out.println(pennycount + " pennies ");
-		} else if (makeChange < 0) {
+			changeDue -= penny;
+		}
+		if (changeDue > .001) {
+			pennycount++;
+		}
+		if (pennycount ==1) {
+			System.out.println(pennycount + " Penny");
+		}else if (pennycount > 0) {
+			System.out.println(pennycount + " pennies");
+		}
+
+		
+		
+		while (changeDue < 0) {
 			System.out.println("no change due");
 		}
-		return makeChange;
-
+		
+		Quit();
+		
+		  } 
+	public static void Quit() { 
+		System.out.println("PleaseComeAgain");
+		  return;
+		 
 	}
 }
-
